623a4ea74e7d2894b56a1a4526643e5de020907a
Ayush <ayushgundawar@gmail.com>
2022-09-25 01:36:21 -0500

    🔨 Added API to CLI

--- reddit-subreddit-scraper/reddit.py
+++ reddit-subreddit-scraper/reddit.py
@@ -4,12 +4,24 @@
 # Imports
 import dataclasses
 import datetime
-import pprint
+import typing
+import uuid
 import dotenv
 import praw
+import typer
+import json
 
 # Load Config Data
 dotenv.load_dotenv()
+config = dotenv.dotenv_values(".env")
+
+# Initialize CLI
+cli = typer.Typer()
+client = praw.Reddit(
+    client_id=config["client_id"],
+    client_secret=config["client_secret"],
+    user_agent=f"mac:{config['client_id']}:v1.0 (by u/{config['username']})",
+)
 
 # Define Objects
 @dataclasses.dataclass
@@ -26,7 +38,7 @@ class Post:
         url: a permanent link to the Reddit post.
         num_comments: the total number of comments on the post.
         top_comments: a list of the scores, authors, and contents of
-          the top 6 comments on the post.
+          the top 10 comments on the post.
     """
 
     post_id: str
@@ -39,159 +51,186 @@ class Post:
     top_comments: list[dict[str, int | str]]
 
 
-class Scraper:
+def __fetch_comments(
+    comments: list[praw.models.Comment],  # type: ignore
+) -> list[dict[str, int | str]]:
     """
-    A scraper that extracts data pertaining to a subreddit.
+    Internal helper for fetching comment data from comment list.
+    """
 
-    Attributes:
-        subreddit_name: the name of the subreddit to scrape.
+    return [
+        {"score": com.score, "author": f"{com.author}", "body": com.body}
+        for com in comments
+        if isinstance(com, praw.models.Comment)  # type: ignore
+    ]
+
+
+def __store_output(result: typing.Any, output_type: str) -> None:
+    """
+    Internal helper for storing command outputs.
+    """
+
+    # Determine file data
+    output_type = output_type.lower()
+    file_id = f"{uuid.uuid4()}"[:8]
+    file_ext = "json" if output_type == "json" else "txt"
+    file_path = f"output/{file_id}.{file_ext}"
+
+    # Write content to file
+    with open(file_path, "w", encoding="utf8") as file:
+        if output_type == "json":
+            json.dump(result, file)
+        else:
+            file.writelines(f"{result}")
+
+    print(f"Successfully stored command output in ./{file_path}")
+
+
+@cli.command(name="keyword_search")
+def search_for_keyword(
+    subreddit_name: str,
+    search_query: str,
+    sorting: str = "hot",
+    interval: str = "day",
+    output: str = "json",
+) -> list[Post]:
+    # sourcery skip: class-extract-method
+    """
+    Search subreddit posts by query.
+
+    Searches subreddit posts for posts containing the search query. Sorts subreddit
+    submissions by hot by default. Searched posts are from the most recent day by default.
+
+    Args:
+        subreddit_name: name of the subreddit to search.
+        search_query: string representing the search query.
+        sorting: string representing the sorting of submissions. Acceptable
+            values are "relevance", "hot", "top", "new", "comments".
+        interval: string representing the time filter for submissions. Acceptable
+            values are "all", "day", "hour", "month", "week", "year".
+        output: controls whether to output data as 'json' or 'dataclass'.
+
+    Returns:
+        A list of reddit.Post objects containing submission data.
 
     Raises:
-        urllib.error.HTTPError: occurs when subreddit_name corresponds to a
-          private/banned subreddit.
+        urllib.error.HTTPError: occurs when the scraper is unable to search posts.
     """
 
-    def __init__(self, subreddit_name: str) -> None:
-        """
-        Initializes the reddit.Scraper object with subreddit name.
-        """
+    # Search subreddit for content
+    subreddit = client.subreddit(subreddit_name.replace("r/", ""))
+    search_results = subreddit.search(
+        query=search_query, sort=sorting, time_filter=interval
+    )
 
-        self.config = dotenv.dotenv_values(".env")
-        self.client = praw.Reddit(
-            client_id=self.config["client_id"],
-            client_secret=self.config["client_secret"],
-            user_agent=f"mac:{self.config['client_id']}:v1.0 (by u/{self.config['username']})",
-        )
-        self.subreddit = self.client.subreddit(subreddit_name.replace("r/", ""))
+    # Store submission data as list of reddit.Post objects
+    post_data = []
+    for submission in search_results:
+        # Configure comment search
+        submission.comment_sort = "confidence"
+        submission.comment_limit = 10
 
-    def __fetch_comments(
-        self, comments: list[praw.models.Comment]
-    ) -> list[dict[str, int | str]]:
-        """
-        Internal helper for fetching comment data from comment list.
-        """
-
-        return [
-            {"score": com.score, "author": com.author, "body": com.body}
-            for com in comments
-            if isinstance(com, praw.models.Comment)
-        ]
-
-    def search_for_keyword(
-        self, search_query: str, sorting: str = "hot", interval: str = "day"
-    ) -> list[Post]:
-        # sourcery skip: class-extract-method
-        """
-        Search subreddit posts by query.
-
-        Searches subreddit posts for posts containing the search query. Sorts subreddit
-        submissions by hot by default. Searched posts are from the most recent day by default.
-
-        Args:
-            search_query: string representing the search query.
-            sorting: string representing the sorting of submissions. Acceptable
-              values are "relevance", "hot", "top", "new", "comments".
-            interval: string representing the time filter for submissions. Acceptable
-              values are "all", "day", "hour", "month", "week", "year".
-
-        Returns:
-            A list of reddit.Post objects containing submission data.
-
-        Raises:
-            urllib.error.HTTPError: occurs when the scraper is unable to search posts.
-        """
-
-        # Search subreddit for content
-        search_results = self.subreddit.search(
-            query=search_query, sort=sorting, time_filter=interval
+        # Create reddit.Post object
+        post = Post(
+            post_id=submission.id,
+            author=f"{submission.author}",
+            score=submission.score,
+            title=submission.title,
+            body=submission.selftext,
+            url=f"https://reddit.com{submission.permalink}",
+            num_comments=submission.num_comments,
+            top_comments=__fetch_comments(submission.comments.list()),
         )
 
-        # Store submission data as list of reddit.Post objects
-        post_data = []
-        for submission in search_results:
+        # Append reddit.Post object to list
+        post_data.append(dataclasses.asdict(post) if output == "json" else post)
+
+    __store_output(post_data, output_type=output)
+    return post_data
+
+
+@cli.command(name="top_posts")
+def posts_from_half_year(subreddit_name: str, output: str = "json") -> list[Post]:
+    """
+    Fetch top posts of the last 26 weeks.
+
+    Searches through the top subreddit posts within the last year. Returns posts
+    created within a 26 week interval from the run date.
+
+    Args:
+        subreddit_name: name of the subreddit to search.
+        output: controls whether to output data as 'json' or 'dataclass'.
+
+    Returns:
+        A list of reddit.Post objects containing submission data.
+
+    Raises:
+        urllib.error.HTTPError: occurs when the scraper is unable to search posts.
+    """
+
+    # Fetch top posts from last year
+    subreddit = client.subreddit(subreddit_name.replace("r/", ""))
+    top_posts = subreddit.top(time_filter="year")
+
+    # Calculate unix epoch time
+    current_time = datetime.datetime.now(datetime.timezone.utc).timestamp()
+
+    # Store submission data as list of reddit.Post objects
+    post_data = []
+    for submission in top_posts:
+        # Filter for submissions from <26 weeks
+        if submission.created_utc > current_time - 15720000:
             # Configure comment search
             submission.comment_sort = "confidence"
-            submission.comment_limit = 6
+            submission.comment_limit = 10
 
-            # Append reddit.Post object to list
-            post_data.append(
-                Post(
-                    post_id=submission.id,
-                    author=submission.author,
-                    score=submission.score,
-                    title=submission.title,
-                    body=submission.selftext,
-                    url=f"https://reddit.com{submission.permalink}",
-                    num_comments=submission.num_comments,
-                    top_comments=self.__fetch_comments(submission.comments.list()),
-                )
+            # Create reddit.Post object
+            post = Post(
+                post_id=submission.id,
+                author=f"{submission.author}",
+                score=submission.score,
+                title=submission.title,
+                body=submission.selftext,
+                url=f"https://reddit.com{submission.permalink}",
+                num_comments=submission.num_comments,
+                top_comments=__fetch_comments(submission.comments.list()),
             )
 
-        return post_data
+            # Append reddit.Post object to list
+            post_data.append(dataclasses.asdict(post) if output == "json" else post)
 
-    def posts_from_half_year(self) -> list[Post]:
-        """
-        Fetch top posts of the last 26 weeks.
+    __store_output(post_data, output_type=output)
+    return post_data
 
-        Searches through the top subreddit posts within the last year. Returns posts
-        created within a 26 week interval from the run date.
 
-        Returns:
-            A list of reddit.Post objects containing submission data.
+@cli.command(name="top_comments")
+def comments_from_half_year(
+    subreddit_name: str, output: str = "json"
+) -> list[list[dict[str, int | str]]]:
+    """
+    Fetch comments from top posts of the last 26 weeks.
 
-        Raises:
-            urllib.error.HTTPError: occurs when the scraper is unable to search posts.
-        """
+    Searches through the top subreddit posts within the last year. Returns top comments
+    of posts created within a 26 week interval from the run date.
 
-        # Fetch top posts from last year
-        top_posts = self.subreddit.top(time_filter="year")
+    Args:
+        subreddit_name: name of the subreddit to search.
+        output: controls whether to output data as 'json' or 'dataclass
 
-        # Calculate unix epoch time
-        current_time = datetime.datetime.now(datetime.timezone.utc).timestamp()
+    Returns:
+        A list of dictionaries containing comment scores, authors, and bodies.
 
-        # Store submission data as list of reddit.Post objects
-        post_data = []
-        for submission in top_posts:
-            # Filter for submissions from <26 weeks
-            if submission.created_utc > current_time - 15720000:
-                # Configure comment search
-                submission.comment_sort = "confidence"
-                submission.comment_limit = 6
+    Raises:
+        urllib.error.HTTPError: occurs when the scraper is unable to search posts.
+    """
 
-                # Append reddit.Post object to list
-                post_data.append(
-                    Post(
-                        post_id=submission.id,
-                        author=submission.author,
-                        score=submission.score,
-                        title=submission.title,
-                        body=submission.selftext,
-                        url=f"https://reddit.com{submission.permalink}",
-                        num_comments=submission.num_comments,
-                        top_comments=self.__fetch_comments(submission.comments.list()),
-                    )
-                )
+    # Get comments from posts
+    top_posts = posts_from_half_year(subreddit_name, output=output)
+    top_comments = [post.top_comments for post in top_posts]
 
-        return post_data
-
-    def comments_from_half_year(self) -> list[dict[str, int | str]]:
-        """
-        Fetch comments from top posts of the last 26 weeks.
-
-        Searches through the top subreddit posts within the last year. Returns top comments
-        of posts created within a 26 week interval from the run date.
-
-        Returns:
-            A list of dictionaries containing comment scores, authors, and bodies.
-
-        Raises:
-            urllib.error.HTTPError: occurs when the scraper is unable to search posts.
-        """
-
-        top_posts = self.posts_from_half_year()
-        return [post.top_comments for post in top_posts]
+    __store_output(top_comments, output_type=output)
+    return top_comments
 
 
 if __name__ == "__main__":
-    scraper = Scraper("r/cryptocurrency")
-    pprint.pprint(scraper.search_for_keyword(search_query=""))
+    cli()
